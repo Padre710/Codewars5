3a404b6de9a2d17a1e230ad81140e7c4d210cad3
Treasure <81287547+Padre710@users.noreply.github.com>
2022-09-27 14:53:32 +0100

    Add project files.

---
+++ Codewars5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+//Write a function that takes a string of parentheses, and determines 
+//if the order of the parentheses is valid. The function should return true
+// if the string is valid, and false if it's invalid.
+
+//Examples
+//"()"              =>  true
+//")(()))"          =>  false
+//"("               =>  false
+//"(())((()())())"  =>  true
+//Constraints
+//0 <= input.length <= 100
+
+//Along with opening (() and closing()) parenthesis, input may contain any valid ASCII characters.
+//Furthermore, the input string may be empty and/or not contain any parentheses at all.
+//Do not treat other forms of brackets as parentheses (e.g. [], { }, <>).
+
+
+namespace Codewars5
+{
+    public class Parentheses
+    {
+        public static bool ValidParentheses(string input)
+        {
+            int parentheses = 0;
+            foreach (char t in input)
+            {
+                if (t == '(')
+                {
+                    parentheses++;
+                }
+                else if (t == ')')
+                {
+                    parentheses--;
+
+                    if (parentheses < 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return parentheses == 0;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Parentheses.ValidParentheses("()")); // true
+            Console.WriteLine(Parentheses.ValidParentheses(")((((")); //false
+            Console.WriteLine(Parentheses.ValidParentheses(")(]()))")); // false
+            Console.WriteLine(Parentheses.ValidParentheses("{(())((()())())")); // true
+            Console.WriteLine(Parentheses.ValidParentheses(" asdfghjhg")); // false
+            Console.WriteLine(Parentheses.ValidParentheses(")(()")); // false
+            Console.WriteLine("********************************************");
+
+            Console.WriteLine(Parentheses.ValidParentheses(" hi(hi) hi(hi))( ")); // false
+
+
+
+        }
+    }
+}
